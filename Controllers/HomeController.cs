744c3526394fb00c301765690ed95a516ef5e84c
Antunigr <antuniribeiro@gmail.com>
2023-04-09 22:55:07 -0300

    firstcommit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using identity.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace identity.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Authorize(Policy = "Name")]
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
